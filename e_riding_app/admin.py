20542d5f5a02892a466f46f7765a52254643e485
sova93 <s_o_va@tut.by>
2017-04-16 18:33:53 +0300

    models fixes

--- e_riding_app/admin.py
+++ e_riding_app/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import DescriptionStep
+from .models import Competition, DescriptionStep, CustomUser, Horse, Team, VetCard, Pair, Step
 
+admin.site.register(Competition)
 admin.site.register(DescriptionStep)
+admin.site.register(CustomUser)
+admin.site.register(Horse)
+admin.site.register(Team)
+admin.site.register(VetCard)
+admin.site.register(Pair)
+admin.site.register(Step)
--- e_riding_app/models.py
+++ e_riding_app/models.py
@@ -8,26 +8,26 @@ class DescriptionStep(models.Model):
 
 
 class CustomUser(AbstractUser):
-    phone = models.TextField()
-    birthday = models.DateField()
+    phone = models.TextField(blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
 
 
 class Horse(models.Model):
     name = models.TextField()
-    owner = models.ForeignKey(CustomUser)
+    owner = models.ForeignKey(CustomUser, related_name='horse_owner')
     color = models.TextField()
     breed = models.TextField()
     stable = models.TextField()
 
 
 class Team(models.Model):
-    rider = models.ForeignKey(CustomUser, related_name='rider_user')
-    horse = models.ForeignKey(Horse)
-    cheerleader = models.ForeignKey(CustomUser, related_name='cheerleader_user')  # cosiak so sviaz'u
+    rider = models.ForeignKey(CustomUser, related_name='team_rider')
+    horse = models.ForeignKey(Horse, related_name='team_horse')
+    cheerleader = models.ManyToManyField(CustomUser, related_name='team_cheerleader')  # cosiak so sviaz'u
 
 
 class VetCard(models.Model):
-    user = models.ForeignKey(CustomUser)  # cosiak so sviaz'u
+    user = models.ForeignKey(CustomUser, related_name='vetcard_user')  # cosiak so sviaz'u
     pulse = models.TextField()
     mucouns_membrane = models.TextField()
     capilary_refill = models.TextField()
@@ -41,11 +41,11 @@ class VetCard(models.Model):
 
 
 class Pair(models.Model):
-    team = models.ForeignKey(Team)
-    rider = models.ForeignKey(CustomUser)
-    horse = models.ForeignKey(Horse)
+    team = models.ForeignKey(Team, related_name='pair_team')
+    rider = models.ForeignKey(CustomUser, related_name='pair_rider')
+    horse = models.ForeignKey(Horse, related_name='pair_horse')
     weight_rider = models.FloatField()
-    vet_card_on_start = models.ForeignKey(VetCard)
+    vet_card_on_start = models.ForeignKey(VetCard, related_name='pair_vet_card_on_start')
 
 
 class Step(models.Model):
@@ -53,6 +53,15 @@ class Step(models.Model):
     finish_time = models.DateTimeField()
     vet_entrance_time = models.DateTimeField()
     norm_execution = models.TextField()
-    pair = models.ForeignKey(Pair)
-    description_step = models.ForeignKey(DescriptionStep)
-    vet_card = models.ForeignKey(VetCard)
+    pair = models.ForeignKey(Pair, related_name='step_pair')
+    description_step = models.ForeignKey(DescriptionStep, related_name='step_description_step')
+    vet_card = models.ForeignKey(VetCard, related_name='step_vet_card')
+
+
+class Competition(models.Model):
+    begin = models.DateTimeField()
+    close = models.DateTimeField()
+    place = models.TextField()
+    manager = models.ForeignKey(CustomUser, related_name='competition_manager')
+    teams = models.ManyToManyField(Team, related_name='competition_teams')
+    judges = models.ManyToManyField(CustomUser, related_name='competition_judges')
